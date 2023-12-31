752c4a8e96aee3e3e71be5ba49466193890eebe8
Ondrej Chvala <ondrejch@gmail.com>
2023-12-31 18:31:38 -0500

    how large the core is, as a function of enrichment

---
+++ 21-CritSearch/core_size_tab.py
@@ -0,0 +1,21 @@
+#!/bin/env python3
+#
+# Prints the size of MCRE-like cores as a function of enrichment
+#
+# Ondrej Chvala <ochvala@utexas.edu>
+#
+# MIT license
+import json5
+
+with open("r_k1s.json") as f:
+    r_scales = json5.load(f)
+
+
+print('# MCRE fuel cylinder model dimensions')
+print('# enr [%]     r [cm]      h/2 [cm]')
+for e, r_scale in r_scales.items():
+    enr = float(e)
+    r0: float = 46.5 / enr
+    r: float = r0 * r_scale
+    h: float = 1.9 * r  # half-height of fuel cyl [cm]
+    print(f'{enr*100.0:4.1f} {r:15.3f} {h:13.3f}')
