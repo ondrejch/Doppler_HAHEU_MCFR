d4c1b6bd720526a490f1ccb260b5fdb00a203152
Ondrej Chvala <ondrejch@gmail.com>
2024-01-12 14:49:30 -0500

    latex

--- 21-CritSearch/core_size_tab_latex.py
+++ 21-CritSearch/core_size_tab_latex.py
@@ -12,10 +12,10 @@ with open("r_k1s.json") as f:
 
 
 print('# MCRE fuel cylinder model dimensions')
-print('# enr [%]     r [cm]      h/2 [cm]')
+print('# enr [%]     D [cm]      H [cm]')
 for e, r_scale in r_scales.items():
     enr = float(e)
     r0: float = 46.5 / enr
     r: float = r0 * r_scale
     h: float = 1.9 * r  # half-height of fuel cyl [cm]
-    print(f'{enr*100.0:4.1f} {r:15.3f} {h:13.3f}')
+    print(f'{enr*100.0:4.1f} &{2.0*r:15.3f} &{2.0*h:13.3f} \\\\')
